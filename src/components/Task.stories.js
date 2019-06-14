40787b088dbe8297d2ee2c67c8320cef8ed29369
Akash Kathiriya <akash.k@redbus.com>
2019-06-14 12:12:38 +0530

    Added redux, knob, task stories

---
+++ src/components/Task.stories.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { withKnobs, object } from "@storybook/addon-knobs/react";
+
+import Task from "./Task";
+
+export const task = {
+  id: "1",
+  title: "New Task",
+  state: "TASK_INBOX",
+  updatedAt: new Date(2018, 0, 1, 9, 0)
+};
+
+export const actions = {
+  onPinTask: action("onPinTask"),
+  onArchiveTask: action("onArchiveTask")
+};
+
+storiesOf("Task", module)
+  .addDecorator(withKnobs)
+  .add("default", () => (
+    <Task task={object("task", { ...task })} {...actions} />
+  ))
+  .add("pinned", () => (
+    <Task
+      task={object("task", { ...task, state: "TASK_PINNED" })}
+      {...actions}
+    />
+  ))
+  .add("archived", () => (
+    <Task
+      task={object("task", { ...task, state: "TASK_ARCHIVED" })}
+      {...actions}
+    />
+  ));
